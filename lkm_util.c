43559801fdca018461e7b55fa1e974c611c54e33
zzgang2008@gmail.com <zzgang2008@gmail.com>
2014-08-20 23:24:21 +0800

    change file names

---
+++ lkm_util.c
@@ -0,0 +1,321 @@
+#include <linux/net.h>
+#include <linux/socket.h>
+#include <linux/in.h>
+#include "sys_call.h"
+#include "lkm_util.h"
+#include "array.h"
+
+/* Poll funcs start */
+struct poll_table_entry_alias {
+    struct file *filp;
+    unsigned long key;
+    wait_queue_t wait;
+    wait_queue_head_t *wait_address;
+};
+
+struct poll_wqueues_alias {
+    poll_table pt;
+    struct poll_table_page *table;
+    struct task_struct *polling_task;
+    int triggered;
+    int error;
+    int inline_index;
+    struct poll_table_entry_alias inline_entries[N_INLINE_POLL_ENTRIES];
+};
+
+struct poll_table_page {
+    struct poll_table_page * next;
+    struct poll_table_entry_alias * entry;
+    struct poll_table_entry_alias entries[0];
+};
+
+#define POLL_TABLE_FULL(table) \
+    ((unsigned long)((table)->entry+1) > PAGE_SIZE + (unsigned long)(table))
+
+static void __pollwait(struct file *filp, wait_queue_head_t *wait_address,
+        poll_table *p);
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 32)
+
+static inline void set_pt_key(poll_table *pt, int events)
+{
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(3, 2, 45)
+    pt->key = events;
+#else
+    pt->_key = events;
+#endif
+}
+
+#endif
+
+#undef DEFAULT_POLLMASK
+#define DEFAULT_POLLMASK (POLLIN | POLLOUT | POLLRDNORM | POLLWRNORM)
+
+static int inline do_poll(struct pollfd_ex_t *pfdt, poll_table *pwait)
+{
+    unsigned int mask;
+    int events;
+    int fd;
+    struct pollfd *pfd;
+
+    pfd = &pfdt->pollfd;
+
+    fd = pfd->fd;
+    events = pfd->events;
+    mask = 0;
+    if (fd >= 0) {
+        struct file * file;
+        struct pollfd_ex_t *pfdt;
+
+        file = lkm_get_file(fd); /*Needn't add f_count*/
+
+        mask = POLLNVAL;
+
+        if (file != NULL) {
+            mask = DEFAULT_POLLMASK;
+
+            events = events|POLLERR|POLLHUP;
+
+            if (file->f_op && file->f_op->poll) {
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 32)
+                if (pwait)
+                    set_pt_key(pwait, events);
+#endif
+
+                pfdt = container_of(pfd, struct pollfd_ex_t, pollfd); 
+
+                if (pfdt->data && pfdt->poll_func)
+                    mask = pfdt->poll_func(pfdt->data, pwait);
+                else 
+                    mask = file->f_op->poll(file, pwait);
+            }
+
+            /* Mask out unneeded events. */
+            mask &= events;
+        }
+    }
+
+    return mask;
+}
+
+static void lkm_poll_initwait(struct poll_wqueues_alias *pwq)
+{
+    init_poll_funcptr(&pwq->pt, __pollwait);
+    pwq->error = 0;
+    pwq->table = NULL;
+    pwq->inline_index = 0;
+    pwq->polling_task = current;
+    pwq->triggered = 0;
+}
+
+static inline void free_poll_entry(struct poll_table_entry_alias *entry)
+{
+    remove_wait_queue(entry->wait_address, &entry->wait);
+}
+
+static void lkm_poll_freewait(struct poll_wqueues_alias *pwq)
+{
+    struct poll_table_page * p = pwq->table;
+    int i;
+
+    for (i = 0; i < pwq->inline_index; i++)
+        free_poll_entry(pwq->inline_entries + i);
+
+    while (p) {
+        struct poll_table_entry_alias * entry;
+        struct poll_table_page *old;
+
+        entry = p->entry;
+        do {
+            entry--;
+            free_poll_entry(entry);
+        } while (entry > p->entries);
+        old = p;
+        p = p->next;
+        free_page((unsigned long) old);
+    }
+}
+
+static struct poll_table_entry_alias *poll_get_entry(struct poll_wqueues_alias *p)
+{
+    struct poll_table_page *table = p->table;
+
+    if (p->inline_index < N_INLINE_POLL_ENTRIES)
+        return p->inline_entries + p->inline_index++;
+
+    if (!table || POLL_TABLE_FULL(table)) {
+        struct poll_table_page *new_table;
+
+        new_table = (struct poll_table_page *) __get_free_page(GFP_ATOMIC);
+        if (!new_table) {
+            p->error = -ENOMEM;
+            return NULL;
+        }
+        new_table->entry = new_table->entries;
+        new_table->next = table;
+        p->table = new_table;
+        table = new_table;
+    }
+
+    return table->entry++;
+}
+
+static int __pollwake(wait_queue_t *wait, unsigned mode, int sync, void *key)
+{
+    struct poll_wqueues_alias *pwq = wait->private;
+    DECLARE_WAITQUEUE(dummy_wait, pwq->polling_task);
+
+    smp_wmb();
+    pwq->triggered = 1;
+
+    return default_wake_function(&dummy_wait, mode, sync, key);
+}
+
+static int pollwake(wait_queue_t *wait, unsigned mode, int sync, void *key)
+{
+    struct poll_table_entry_alias *entry;
+
+    entry = container_of(wait, struct poll_table_entry_alias, wait);
+    if (key && !((unsigned long)key & entry->key))
+        return 0;
+
+    return __pollwake(wait, mode, sync, key);
+}
+
+/* Add a new entry */
+static void __pollwait(struct file *filp, wait_queue_head_t *wait_address,
+        poll_table *p)
+{
+    struct poll_wqueues_alias *pwq = container_of(p, struct poll_wqueues_alias, pt);
+    struct poll_table_entry_alias *entry = poll_get_entry(pwq);
+
+    if (!entry)
+        return;
+
+    entry->filp = filp;
+    entry->wait_address = wait_address;
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 32)
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(3, 2, 45)
+    entry->key = p->key;
+#else
+    entry->key = p->_key;
+#endif
+#endif
+
+    init_waitqueue_func_entry(&entry->wait, pollwake);
+    entry->wait.private = pwq;
+
+    add_wait_queue(wait_address, &entry->wait);
+}
+
+int lkm_poll(array_t *pfdt_list, int timeo)
+{
+    struct poll_wqueues_alias table;
+    poll_table *pt;
+    int count = 0;
+    int timed_out = 0;
+    long __timeout = timeo * HZ;
+
+    lkm_poll_initwait(&table);
+    pt = &(&table)->pt;
+    for (;;) {
+        int idx;
+
+        if (!(pfdt_list))
+            goto ignore_poll;
+
+        for (idx = 0; idx < (pfdt_list)->elements; idx++) {
+            struct pollfd_ex_t *pfdt;
+            struct pollfd *pfd;
+
+            pfdt = (struct pollfd_ex_t *)(pfdt_list)->get(pfdt_list, idx);
+
+            pfd = (struct pollfd *)&pfdt->pollfd;
+
+            pfd->revents = do_poll(pfdt, pt);
+
+            if (pfd->revents) {
+
+                count++;
+                pt = NULL;
+
+            }
+
+        }
+
+ignore_poll:
+
+        pt = NULL;
+
+        if (!count) {
+            if (signal_pending(current)) {
+                count = -EINTR;
+                flush_signals(current);
+            }
+        }
+
+        if (count || timed_out)
+            break;
+
+        __timeout = wait_for_sig_or_timeout(__timeout);
+        if (!__timeout)
+            timed_out = 1;
+    }
+
+    lkm_poll_freewait(&table); 
+
+    return count;
+}
+/* Poll funcs END */
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 7, 10)
+static int sock_map_fd(struct socket *sock, int flags)
+{
+    struct file *newfile;
+    int fd;
+
+    fd = get_unused_fd_flags(flags);
+    if (unlikely(fd < 0))
+        return fd;
+
+    newfile = sock_alloc_file(sock, flags, NULL);
+    if (likely(!IS_ERR(newfile))) {
+        fd_install(fd, newfile);
+        return fd;
+    }
+
+    put_unused_fd(fd);
+    return PTR_ERR(newfile);
+}
+#endif
+
+int lkm_create_tcp_connect(struct sockaddr_in *address)
+{
+    int fd;
+    struct socket *sock;
+    int err;
+
+    fd = sock_create(address->sin_family, SOCK_STREAM, 0, &sock);
+    if (fd < 0)
+        return fd;
+
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(2, 6, 26)
+    fd = sock_map_fd(sock);
+#else
+    fd = sock_map_fd(sock, 0);
+#endif
+    if (fd < 0) {
+        sock_release(sock);
+        return fd;
+    }
+
+    sock->file->f_flags |= O_NONBLOCK;
+
+    err = sock->ops->connect(sock, (struct sockaddr *)address,
+            sizeof(struct sockaddr), sock->file->f_flags);
+
+    SET_CLIENT_FLAG(sock);
+
+    return fd;
+}
---
+++ lkm_util.h
@@ -0,0 +1,376 @@
+/*
+ * Most of the codes for this file is brought from kernel source code.
+ * 
+ */
+
+#ifndef _UTIL_H
+#define _UTIL_H
+
+#include <linux/version.h>
+#include <linux/file.h>
+#include <linux/fs.h>
+#include <linux/net.h>
+#include <net/sock.h>
+#include <linux/stat.h>
+#include <linux/sched.h>
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 32)
+#include <linux/fdtable.h>
+#endif
+
+#include <asm/atomic.h>
+#include <linux/slab.h>
+#include <linux/rcupdate.h>
+#include <linux/list.h>
+#include <linux/poll.h>
+#include <linux/jiffies.h>
+
+#define wait_for_sig_or_timeout(timeout) schedule_timeout_interruptible(timeout)
+#define wait_for_timeout(timeout) schedule_timeout_uninterruptible(timeout)
+
+struct pollfd_ex_t {
+    struct pollfd pollfd;
+    void *data;
+    int (*poll_func)(void *data, poll_table *pt);
+};
+
+#define MIN(arg1, arg2) (arg1 < arg2 ? arg1 : arg2)
+#define MAX(arg1, arg2) (arg1 > arg2 ? arg1 : arg2)
+
+#define E_EVENTS (POLLERR|POLLHUP|POLLNVAL)
+
+#define NOW_SECS (CURRENT_TIME_SEC.tv_sec)
+
+#define NOTIFY_SIG (SIGINT)
+#define SEND_FORCE (1)
+#define notify(tskp) send_sig(NOTIFY_SIG, (tskp), SEND_FORCE)
+
+#define INVOKED_BY_TGROUP_LEADER() (current == current->group_leader)
+
+#define lkmalloc(size) kzalloc(size, GFP_ATOMIC)
+#define lkmfree(ptr) kfree(ptr)
+
+#define BYTES_ALIGN(size) (((size) + (sizeof(long) - 1)) & ~(sizeof(long) - 1))
+
+#define SOCK_CLIENT_TAG (1U << 30)
+
+#define IS_CLIENT_SOCK(sock)                    \
+    ((sock)->file && ((sock)->file->f_flags & SOCK_CLIENT_TAG))
+
+#define SET_CLIENT_FLAG(sock) do {              \
+    if ((sock)->file)                           \
+    (sock)->file->f_flags |= SOCK_CLIENT_TAG;   \
+} while (0)
+
+#define CLEAR_CLIENT_FLAG(sock) do {            \
+    if ((sock)->file)                           \
+    (sock)->file->f_flags &= ~SOCK_CLIENT_TAG;  \
+} while (0)
+
+#define SK_ESTABLISHING(sk) \
+    (sk->sk_state == TCP_SYN_SENT)
+
+#define SK_ESTABLISHED(sk)  \
+    (sk->sk_state == TCP_ESTABLISHED)
+
+#define SET_SOCK_STATE(sock, STATE)    \
+    ((sock)->state = STATE)
+
+#define SOCK_ESTABLISHED(sock) \
+    ((sock)->sk && SK_ESTABLISHED((sock)->sk))
+
+#define IS_TCP_SOCK(sock) \
+    ((sock)->type == SOCK_STREAM)
+
+#define IS_UNCONNECTED_SOCK(sock) \
+    ((sock)->type == SS_UNCONNECTED)
+
+#define lkm_atomic32_read(v) atomic_read((atomic_t *)v) 
+#define lkm_atomic32_add(v, a) atomic_add_return(a, (atomic_t *)v)
+#define lkm_atomic32_sub(v, a) atomic_sub_return(a, (atomic_t *)v)
+#define lkm_atomic32_set(v, a) atomic_set((atomic_t *)v, a) 
+
+#define lkm_atomic64_read(v) atomic64_read((atomic64_t *)v) 
+#define lkm_atomic64_add(v, a) atomic64_add_return(a, (atomic64_t *)v)
+#define lkm_atomic64_sub(v, a) atomic64_sub_return(a, (atomic64_t *)v)
+#define lkm_atomic64_set(v, a) atomic64_set((atomic64_t *)v, a) 
+
+#if BITS_PER_LONG < 64 //32 bits
+
+typedef atomic_t lkm_atomic_t;
+#define lkm_atomic_read(v) lkm_atomic32_read(v)
+#define lkm_atomic_add(v, a) lkm_atomic32_add(v, a)
+#define lkm_atomic_sub(v, a) lkm_atomic32_sub(v, a)
+#define lkm_atomic_set(v, a) lkm_atomic32_set(v, a)
+
+#else //64bits
+
+typedef atomic64_t lkm_atomic_t;
+#define lkm_atomic_read(v) lkm_atomic64_read(v)
+#define lkm_atomic_add(v, a) lkm_atomic64_add(v, a)
+#define lkm_atomic_sub(v, a) lkm_atomic64_sub(v, a)
+#define lkm_atomic_set(v, a) lkm_atomic64_set(v, a)
+
+#endif
+
+#define lkm_get_file(fd)            \
+    ({ struct file * __file;    \
+     rcu_read_lock();       \
+     __file = fcheck_files(TASK_FILES(current), fd); \
+     rcu_read_unlock(); \
+     __file;})
+
+#define TASK_FILES(tsk) (tsk)->files
+
+#define FILE_FDT_TYPE typeof(((struct files_struct *)0)->fdtab)
+
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(2, 6, 32)
+
+#define TASK_FILES_FDT(tsk) ({   \
+        FILE_FDT_TYPE * __tmp;      \
+        rcu_read_lock();            \
+        __tmp = rcu_dereference(TASK_FILES(tsk)->fdt);  \
+        rcu_read_unlock();  \
+        __tmp;})
+
+#else
+
+#define TASK_FILES_FDT(tsk) ({   \
+        FILE_FDT_TYPE * __tmp;      \
+        rcu_read_lock();            \
+        __tmp = rcu_dereference_check_fdtable(TASK_FILES(tsk), TASK_FILES(tsk)->fdt);  \
+        rcu_read_unlock();  \
+        __tmp;})
+
+#endif
+
+#define FDT_GET_FILE(fdt) ({    \
+        struct file *__tmp;     \
+        rcu_read_lock();        \
+        __tmp = rcu_dereference(fdt->fd[fd]);   \
+        rcu_read_unlock();  \
+        __tmp;})
+
+#define lkm_setrlimit(resource, new_rlim)                       \
+    do {                                                        \
+        struct rlimit *old_rlim;                                \
+                                                                \
+        old_rlim = current->signal->rlim + resource;            \
+        *old_rlim = new_rlim;                                   \
+    } while (0)
+
+typedef struct array_t array_t;
+extern int lkm_poll(array_t *, int timeout);
+
+#define lkm_jiffies (unsigned)jiffies
+
+//Compat for 32-bits jiffies
+static inline u64 lkm_jiffies_elapsed_from(u64 from)
+{
+    s64 elapsed_jiffies = lkm_jiffies - from;
+
+    return elapsed_jiffies >= 0 ? elapsed_jiffies : (elapsed_jiffies + ULONG_MAX);
+}
+
+static inline int file_count_read(struct file *filp)
+{
+    return lkm_atomic32_read(&filp->f_count);
+}
+
+static inline int file_count_inc(struct file *filp)
+{
+    return lkm_atomic32_add(&filp->f_count, 1);
+}
+
+static inline int file_count_dec(struct file *filp)
+{
+    return lkm_atomic32_sub(&filp->f_count, 1);
+}
+
+static inline void file_count_set(struct file *filp, int c)
+{
+    lkm_atomic32_set(&filp->f_count, c);
+}
+
+struct fd_entry {
+    int fd;
+    struct list_head siblings;
+};
+
+static inline int lkm_get_unused_fd(void)
+{
+    int fd;
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 7, 10)
+    fd = get_unused_fd_flags(0);
+#else
+    fd = get_unused_fd();
+#endif
+
+    return fd;
+}
+
+struct sockaddr_in;
+extern int lkm_create_tcp_connect(struct sockaddr_in *);
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 7, 10)
+extern int lkm_sock_map_fd(struct socket *sock, int flags);
+#endif
+
+static inline void TASK_GET_FDS(struct task_struct *tsk, struct list_head *fds_list)
+{
+    int i, j = 0;
+    FILE_FDT_TYPE *fdt;
+    
+    fdt = TASK_FILES_FDT(tsk);
+    
+    for (;;) {
+        unsigned long set;
+        struct fd_entry *tmp;
+
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(3, 2, 45)
+        i = j * __NFDBITS;
+#else
+        i = j * BITS_PER_LONG;
+#endif
+
+        if (i >= fdt->max_fds)
+            break;
+
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(3, 2, 45)
+        set = fdt->open_fds->fds_bits[j++];
+#else
+        set = fdt->open_fds[j++];
+#endif
+
+        while (set) {
+
+            if (set & 1) {
+                /*Collect the fds list and must free mem space by caller.*/
+                tmp = (typeof(*tmp) *)lkmalloc(sizeof(typeof(*tmp)));
+                tmp->fd = i;
+                list_add_tail(&tmp->siblings, fds_list);
+            }
+            i++;
+            set >>= 1;
+
+        }
+    }
+}
+
+static inline void task_fd_install(struct task_struct *tsk, int fd, struct file *filp)
+{
+    FILE_FDT_TYPE *fdt;
+    struct files_struct *files = TASK_FILES(tsk);
+
+    spin_lock(&files->file_lock);
+
+    fdt = TASK_FILES_FDT(tsk);
+    rcu_assign_pointer(fdt->fd[fd], filp);
+
+    spin_unlock(&files->file_lock);
+}
+
+
+static inline struct socket *getsock(int fd)
+{
+    struct file *filp;
+    FILE_FDT_TYPE *fdt = TASK_FILES_FDT(current);
+
+    filp = FDT_GET_FILE(fdt);
+    if (filp)
+        return (struct socket *)filp->private_data;
+
+    return NULL;
+}
+
+static inline int getsockservaddr(struct socket *sock, struct sockaddr *address)
+{
+    int len;
+    int err;
+
+    if (!sock->sk)
+        return 0;
+
+    err = sock->ops->getname(sock, address, &len, 1);
+    if (err)
+        return 0;
+
+    return 1;
+}
+
+static inline int is_sock_fd(int fd)
+{
+    struct kstat statbuf;
+
+    if(vfs_fstat(fd, &statbuf) < 0)
+        return 0;
+
+    return S_ISSOCK(statbuf.mode);
+}
+
+static inline void sock_destroy(struct sock *sk)
+{
+    sock_orphan(sk);
+    sk_free(sk);
+}
+
+static inline time_t get_fmtime(char *fname)
+{
+    struct kstat statbuf;
+
+    if (vfs_stat(fname, &statbuf) < 0)
+        return 0;
+
+    return statbuf.mtime.tv_sec; 
+}
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 26)
+
+static inline void set_page_rw(unsigned long addr) 
+{
+    unsigned int level;
+
+    pte_t *pte = lookup_address(addr, &level);
+
+    if (pte->pte & ~_PAGE_RW) 
+        pte->pte |= _PAGE_RW;
+}
+
+static inline void set_page_ro(unsigned long addr) 
+{
+    unsigned int level;
+
+    pte_t *pte = lookup_address(addr, &level);
+    pte->pte &= ~_PAGE_RW;
+}
+
+#else
+
+static inline void disable_page_protection(void) 
+{
+    unsigned long value;
+
+    asm volatile("mov %%cr0,%0" : "=r" (value));
+
+    if (value & 0x10000UL) {
+        value &= ~0x10000UL;
+        asm volatile("mov %0,%%cr0": : "r" (value));
+    }
+}
+
+static inline void enable_page_protection(void) 
+{
+    unsigned long value;
+
+    asm volatile("mov %%cr0,%0" : "=r" (value));
+
+    if (!(value & 0x10000UL)) {
+        value |= 0x10000UL;
+        asm volatile("mov %0,%%cr0": : "r" (value));
+    }
+}
+
+#endif //end KERNEL_VERSION
+
+#endif
