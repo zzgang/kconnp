ae1764779ee81adc0ee9e2c16b26718d42b94aa3
zzgang2008@gmail.com <zzgang2008@gmail.com>
2016-02-18 14:04:56 +0800

    compitible for kernel version(>=4.2.0)

--- connpd.c
+++ connpd.c
@@ -170,6 +170,10 @@ static int connpd_func(void *data)
     
     allow_signal(NOTIFY_SIG);
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 2, 0)
+        init_waitqueue_head(&current->files->resize_wait);
+#endif
+
     for(;;) {
 
         if (kthread_should_stop()) {
