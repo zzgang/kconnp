2e8e3fc5a8ce94c3bdc4fee37b41391088f57422
zzgang2008@gmail.com <zzgang2008@gmail.com>
2017-03-11 19:52:42 -0500

    Change function name

--- sys_socketcalls_S.c
+++ sys_socketcalls_S.c
@@ -8,7 +8,7 @@ asmlinkage long connp_sys_poll(struct pollfd __user *ufds, unsigned int nfds,
     if (nfds == 1) {
         struct pollfd pfd;
         u32 retcnt;
-        if (lkm_copy_from_user(&pfd, ufds, sizeof(struct pollfd)))
+        if (copy_from_user(&pfd, ufds, sizeof(struct pollfd)))
             goto orig_poll;
 
         if (!(pfd.events & POLLIN)) 
